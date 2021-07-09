5518d83915a51965faf9eb85eaddb270ef901751
Mauricio Meza <mmezab@unal.edu.co>
2021-07-08 23:55:46 -0500

    added sliders for normal, rough, metal, light

--- 3d.js
+++ 3d.js
@@ -1,15 +1,18 @@
 //----Initial Setup
+var w = 1.4
+var h = 1.03
+
+container = document.getElementById( 'canvas3d' );
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
 	75, //Field of View
-	window.innerWidth/ window.innerHeight, //Aspect Ratio
+	(window.innerWidth/w)/ (window.innerHeight/h), //Aspect Ratio
 	0.1,  //Inner Frustum
 	1000 //Outter Frustum
 );
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
+renderer.setSize((window.innerWidth)/w, window.innerHeight/h);
+container.appendChild( renderer.domElement );
 
 
 //----Control Setup
@@ -22,19 +25,17 @@ controls.update();
 const textureLoader = new THREE.TextureLoader();
 	//Load Texture Maps for the object
 	const diffuse = textureLoader.load("./3DFiles/45634.png");
-	const metallic = textureLoader.load("./3DFiles/45634-mr.png");
 	const roughness = textureLoader.load("./3DFiles/45634-rg.png");
 	const normal = textureLoader.load("./3DFiles/45634-n.png");
 	const height = textureLoader.load("./3DFiles/45634-h.png");
 
 
-//----Geometry Setup
+//----Geometry and Material Setup
 const geometry = new THREE.SphereGeometry( 2, 32, 32);
-
 const material = new THREE.MeshStandardMaterial( {
 	map: diffuse, 
 	roughnessMap: roughness, 
-	metalnessMap: metallic,
+	metalness: 0.25,
 	normalMap: normal,
 	normalScale: new THREE.Vector2( .35, .35 ),
 	envMapIntensity: 1.0
@@ -66,6 +67,33 @@ function animate(){
 	sphere.rotation.y += .01;
  	controls.update();
 	renderer.render(scene, camera);
+}
+
+var sliderNormal = document.getElementById("normalRange");
+sliderNormal.oninput = function() {
+	val = this.value/100
+	sphere.material.normalScale = new THREE.Vector2( val, val ); 
+}
+
+var sliderRoughness = document.getElementById("roughnessRange");
+sliderRoughness.oninput = function() {
+	val = this.value/50
+	if(val > 1){
+		val = val*val;
+	}
+	sphere.material.roughness = val; 
+}
+
+var sliderMetal = document.getElementById("metalRange");
+sliderMetal.oninput = function() {
+	val = this.value/200;
+	sphere.material.metalness = val; 
+}
+
+var sliderAmbient = document.getElementById("ambientRange");
+sliderAmbient.oninput = function() {
+	val = this.value/100
+	sphere.material.envMapIntensity = val; 
 }
 
 animate();
